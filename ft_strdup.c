a64e65287ef53431e0860842bd14838c4abde690
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 10:33:43 +0300

    string: strdup

---
+++ ft_strdup.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 08:43:51 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 10:33:03 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <stdio.h>
+
+static int	ft_strlen(const char *s)
+{
+	int	index;
+
+	index = 0;
+	while (s[index])
+		index++;
+	return (index);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		index;
+	int		s_len;
+	char	*dst;
+
+	dst = 0;
+	index = 0;
+	s_len = ft_strlen(s);
+	dst = malloc(sizeof(char) * s_len);
+	if (dst == 0)
+		return (0);
+	while (s[index])
+	{
+		dst[index] = s[index];
+		index++;
+	}
+	return (dst);
+}
+
+/*
+int	main(void)
+{
+	char	*str;
+	char	*n_str;
+
+	str = "Hello world";
+	n_str = ft_strdup(str);
+	printf("dup: [%s] in [%p]\n", str, str);
+	printf("res: [%s] in [%p]\n", n_str, n_str);
+	free(n_str);
+	return (0);
+}
+*/
