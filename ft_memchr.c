11d8b291684f2539445d10811a51adcd87759a0a
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 13:31:56 +0300

    string: memchr

---
+++ ft_memchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 13:14:56 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 13:31:36 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int	index;
+
+	index = 0;
+	while (index < (int)n && ((char *)s)[index] != (char)c)
+		index++;
+	if (((char *)s)[index] == (char)c)
+		return ((char *)s + index);
+	return (0);
+}
+
+/*
+int	main(void)
+{
+	char	*str;
+	int		len;
+	char	c;
+
+	c = 'w';
+	len = 12;
+	str = "Hello\0world";
+	printf("str: [%p]\n", str);
+	printf("[%c] found at [%p]\n", c, ft_memchr(str, c, len));
+	printf("[%c] found at [%p]\n", c, memchr(str, c, len));
+	return (0);
+}
+*/
