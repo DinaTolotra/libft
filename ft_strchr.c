5f578b53821d28e029e476eba1fbfb547d3aefeb
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 10:27:42 +0300

    string: strchr

---
+++ ft_strchr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 08:44:51 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 10:27:21 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <string.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	index;
+
+	index = 0;
+	while (s[index] && s[index] != c)
+		index++;
+	if (s[index])
+		return ((char *)(s + index));
+	return (0);
+}
+
+/*
+int	main(void)
+{
+	char	c;
+	char	*str;
+
+	c = 'o';
+	str = "Hello world";
+	printf("[%c] found in [%s] at [%li]\n", c, str, ft_strchr(str, c) - str);
+	return (0);
+}
+*/
