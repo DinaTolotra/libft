04ab5165408bd99c592d64029b286146b0ea9757
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 09:49:06 +0300

    ctype: tolower

---
+++ ft_tolower.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 08:31:46 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 09:50:51 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+static int	ft_isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+int	ft_tolower(int c)
+{
+	if (ft_isupper(c))
+		return (c - 'A' + 'a');
+	return (c);
+}
+
+/*
+int	main(void)
+{
+	int		index;
+	char	*str;
+
+	index = 0;
+	str = "HeLlO";
+	printf("str:\t%s\nlower:\t", str);
+	while (str[index])
+	{
+		putchar(ft_tolower(str[index]));
+		index++;
+	}
+	putchar('\n');
+	return (0);
+}
+*/
