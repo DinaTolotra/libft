93037fb5dd85777532fc27faf80ff8b88d910a9e
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 11:20:33 +0300

    string: strlen

---
+++ ft_strlen.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 08:43:17 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 11:20:16 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <string.h>
+//#include <stdio.h>
+
+int	ft_strlen(const char *s)
+{
+	int	index;
+
+	index = 0;
+	while (s[index])
+		index++;
+	return (index);
+}
+
+/*
+int	main(void)
+{
+	char	*str;
+	int		len;
+
+	str = "Hello world";
+	len = ft_strlen(str);
+	printf("len: [%s] [%i]\n", str, len);
+	len = strlen(str);
+	printf("len: [%s] [%i]\n", str, len);
+	return (0);
+}
+*/
