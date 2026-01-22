a10e2bb0c69ea0869af859b41c725166254abe6d
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 23:17:52 +0300

    list: lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:35:05 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 23:10:51 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/22 23:14:56 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
 
 #endif // LIBFT_H
