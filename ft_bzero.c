021a16adcc4052539436ff14b82c777e2948f955
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-22 12:58:43 +0300

    string: bzero

---
+++ ft_bzero.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 12:50:59 by todina-r          #+#    #+#             */
+/*   Updated: 2026/01/22 12:58:12 by todina-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	int	index;
+
+	index = 0;
+	while (index < (int)n)
+	{
+		((char *)s)[index] = 0;
+		index++;
+	}
+}
+
+/*
+int	main(void)
+{
+	char	*buff;
+	int		index;
+	int		len;
+
+	index = 0;
+	buff = strdup("Hello");
+	len = strlen(buff);
+	while (index < len)
+	{
+		printf("%i ", (int)buff[index]);
+		index++;
+	}
+	putchar('\n');
+	index = 0;
+	ft_bzero(buff, len);
+	while (index < len)
+	{
+		printf("%i ", (int)buff[index]);
+		index++;
+	}
+	putchar('\n');
+	free(buff);
+	return (0);
+}
+*/
