9bfec2c993947645023bebd2aa14543e776148f8
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-27 09:47:33 +0300

    string: strlcpy correction

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:43:26 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 15:42:45 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/27 09:40:33 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,20 @@
 
 int	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int	index;
-	int	src_len;
+	size_t	index;
+	int		len;
 
 	index = 0;
-	src_len = ft_strlen(src);
-	while (src[index] && index < (int)(size - 1))
+	len = ft_strlen(src);
+	if (size == 0)
+		return (len);
+	while (src[index] && index < size - 1)
 	{
 		dst[index] = src[index];
 		index++;
 	}
-	if (size > 0)
-		dst[index] = 0;
-	return (src_len);
+	dst[index] = 0;
+	return (len);
 }
 
 /*
@@ -38,10 +39,10 @@ int	main(void)
 	char	str[2];
 	int		ret;
 
-	ret = ft_strlcpy(str, "Hello", 2);
+	ret = strlcpy(str, "Hello", 0);
 	printf("str: [%s] [%i]\n", str, ret);
-	ret = strlcpy(str, "", 2);
-	ret = strlcpy(str, "Hello", 2);
+	ret = strlcpy(str, "", 0);
+	ret = ft_strlcpy(str, "Hello", 0);
 	printf("str: [%s] [%i]\n", str, ret);
 	return (0);
 }
