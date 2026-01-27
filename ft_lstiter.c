4af88b6810869f981cba54b9e7cf23623bdd3672
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-27 22:34:50 +0300

    lstiter: null handling

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 23:44:28 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 23:45:51 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/27 22:34:39 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (lst)
 	{
-		f(lst->content);
+		if (f)
+			f(lst->content);
 		ft_lstiter(lst->next, f);
 	}
 }
