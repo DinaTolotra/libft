20019033eafaf3f03b3904370f56d591887030c5
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-27 22:20:32 +0300

    split: null handling

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:49:36 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 21:44:23 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/27 21:22:36 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,17 @@ static int	count_separated(char const *s, char c)
 	end = 0;
 	beg = -1;
 	count = 0;
-	while (s[end])
+	while (s[end] || beg != -1)
 	{
 		if (s[end] != c && beg == -1)
 			beg = end;
-		if (s[end] == c && beg != -1)
+		if ((s[end] == 0 || s[end] == c) && beg != -1)
 		{
 			count++;
 			beg = -1;
 		}
-		end++;
+		end = end + (s[end] != 0);
 	}
-	if (beg != -1)
-		count++;
 	return (count);
 }
 
@@ -49,21 +47,21 @@ char	**ft_split(char const *s, char c)
 	end = 0;
 	beg = -1;
 	res_i = 0;
+	if (s == 0)
+		return (0);
 	count = count_separated(s, c);
 	res = ft_calloc(sizeof(char *), (count + 1));
-	while (s[end])
+	while (s[end] || beg != -1)
 	{
 		if (s[end] != c && beg == -1)
 			beg = end;
-		if (s[end] == c && beg != -1)
+		if ((s[end] == 0 || s[end] == c) && beg != -1)
 		{
 			res[res_i++] = ft_substr(s, beg, end - beg);
 			beg = -1;
 		}
-		end++;
+		end = end + (s[end] != 0);
 	}
-	if (beg != -1)
-		res[res_i++] = ft_substr(s, beg, end - beg);
 	return (res);
 }
 
