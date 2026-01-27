619b98b736f031a78e736ad2a6bafefcd7d9c2b6
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-27 16:32:38 +0300

    string: strtrim correction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,37 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:30:48 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 22:32:32 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/27 16:32:07 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
 #include "./libft.h"
+
+static int	count_leftchar(char const *s, char const *set)
+{
+	int	count;
+	int	index;
+	int	set_i;
+	int	valid;
+
+	count = 0;
+	index = 0;
+	while (s[index])
+	{
+		set_i = 0;
+		valid = 1;
+		while (set[set_i] && valid == 1)
+		{
+			if (set[set_i] == s[index])
+				valid = 0;
+			set_i++;
+		}
+		count = count + valid;
+		index++;
+	}
+	return (count);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -22,8 +47,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	res_i = 0;
 	index = 0;
-	len = ft_strlen(s1);
-	res = malloc(sizeof(char) * len);
+	len = count_leftchar(s1, set);
+	res = malloc(sizeof(char) * (len + 1));
 	while (s1[index])
 	{
 		if (ft_strchr(set, s1[index]) == 0)
