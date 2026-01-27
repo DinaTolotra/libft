837bc9a64aedf8d34972b57c09df6a30c781d1e5
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-27 16:25:01 +0300

    string: substr correction

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,26 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 15:57:18 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 22:31:23 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/27 16:24:35 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 #include "./libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub_s;
+	size_t	s_len;
 
-	sub_s = malloc(sizeof(char) * (len + 1));
-	ft_strlcpy(sub_s, s + start, len + 1);
+	s_len = ft_strlen(s);
+	if (s_len - start < len)
+		len = s_len - start;
+	if (s_len < start)
+		len = 0;
+	sub_s = calloc(sizeof(char), (len + 1));
+	if (start < s_len)
+		ft_strlcpy(sub_s, s + start, len + 1);
 	sub_s[len] = 0;
 	return (sub_s);
 }
