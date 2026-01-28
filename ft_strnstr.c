37cf422c56df0dafc2743fd80fbd90c002fd3768
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 22:18:16 +0300

    strnstr: relocating the return statment

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,11 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:45:46 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 18:51:15 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:17:14 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <bsd/string.h>
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
@@ -36,20 +33,3 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (res);
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-	char	*res;
-	char	*sub_str;
-
-	str = "hello world";
-	sub_str = "rl";
-	res = ft_strnstr(str, sub_str, 12);
-	printf("[%s] found in [%s] at [%p]\n", sub_str, str, res);
-	res = strnstr(str, sub_str, 12);
-	printf("[%s] found in [%s] at [%p]\n", sub_str, str, res);
-	return (0);
-}
-*/
