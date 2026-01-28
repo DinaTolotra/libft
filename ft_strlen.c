cbbc21dfa46918cadfaacde8d1455a8f0f1250d0
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 19:14:52 +0300

    strlen return type correction

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,15 +6,13 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:43:17 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 14:50:36 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 19:12:54 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
-int	ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	int	index;
 
@@ -23,18 +21,3 @@ int	ft_strlen(const char *s)
 		index++;
 	return (index);
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-	int		len;
-
-	str = "Hello world";
-	len = ft_strlen(str);
-	printf("len: [%s] [%i]\n", str, len);
-	len = strlen(str);
-	printf("len: [%s] [%i]\n", str, len);
-	return (0);
-}
-*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:35:05 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 09:02:22 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 19:14:17 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-int		ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strlcpy(char *dst, const char *src, size_t size);
 int		ft_strlcat(char *dst, const char *src, size_t size);
