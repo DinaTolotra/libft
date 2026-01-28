29e578c9de49c0e48986ecd6ceff179fb08636d1
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 22:10:15 +0300

    memcmp: relocating the return statment & improve readability

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,50 +6,27 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 13:33:02 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 18:50:45 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:09:17 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-#include <stdio.h>
-#include "./libft.h"
-
-void	print_mem(const void *p, size_t n)
-{
-	size_t	index;
-
-	index = 0;
-	while (index < n)
-	{
-		printf("[%i] ", ((char *)p)[index]);
-		index++;
-	}
-}
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	index;
-	int		res;
+	size_t				index;
+	int					res;
+	const unsigned char	*s_s1;
+	const unsigned char	*s_s2;
 
 	res = 0;
 	index = 0;
-	while (res == 0 && index < n)
+	s_s1 = s1;
+	s_s2 = s2;
+	while (index < n && res == 0)
 	{
-		res = ((unsigned char *)s1)[index] - ((unsigned char *)s2)[index];
+		res = s_s1[index] - s_s2[index];
 		index++;
 	}
 	return (res);
 }
-
-/*
-int	main(void)
-{
-	char	*buff1;
-	char	*buff2;
-
-	buff1 = "a\0bb";
-	buff2 = "a\0Cb";
-	printf("[%i]\n", ft_memcmp(buff1, buff2, 4));
-	return (0);
-}
-*/
