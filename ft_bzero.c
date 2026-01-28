201c799d47c52c1d157e4f6674609726f00c2d71
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 21:03:35 +0300

    bzero: removing commented test & shortening the code

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,51 +6,13 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 12:50:59 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 13:37:37 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 20:43:54 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	index;
-
-	index = 0;
-	while (index < n)
-	{
-		((char *)s)[index] = 0;
-		index++;
-	}
+	ft_memset(s, 0, n);
 }
-
-/*
-int	main(void)
-{
-	char	*buff;
-	int		index;
-	int		len;
-
-	index = 0;
-	buff = strdup("Hello");
-	len = strlen(buff);
-	while (index < len)
-	{
-		printf("%i ", (int)buff[index]);
-		index++;
-	}
-	putchar('\n');
-	index = 0;
-	ft_bzero(buff, len);
-	while (index < len)
-	{
-		printf("%i ", (int)buff[index]);
-		index++;
-	}
-	putchar('\n');
-	free(buff);
-	return (0);
-}
-*/
