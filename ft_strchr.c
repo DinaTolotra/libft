75ab9ebe64dbed4457de1e0906930b92c6126540
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 21:24:49 +0300

    strchr: removing commented test

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,36 +6,22 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:44:51 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 10:24:18 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 21:23:59 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		index;
-	char	*res;
+	int			index;
+	const char	*res;
 
 	res = 0;
 	index = 0;
 	while (s[index] && s[index] != (char)c)
 		index++;
 	if (s[index] == (char)c)
-		res = ((char *)(&s[index]));
-	return (res);
+		res = &s[index];
+	return ((char *)res);
 }
-
-/*
-int	main(void)
-{
-	char	c;
-	char	*str;
-
-	c = 'o';
-	str = "Hello world";
-	printf("[%c] found in [%s] at [%p]\n", c, str, ft_strchr(str, c));
-	return (0);
-}
-*/
