abb77302a7450feeebb63965078171e00ec6f2a2
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 21:22:46 +0300

    toupper & tolower: removed commented test

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,12 +6,11 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:31:46 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 15:17:35 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 21:22:04 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 static int	ft_isupper(int c)
 {
@@ -24,22 +23,3 @@ int	ft_tolower(int c)
 		return (c - 'A' + 'a');
 	return (c);
 }
-
-/*
-int	main(void)
-{
-	int		index;
-	char	*str;
-
-	index = 0;
-	str = "HeLlO";
-	printf("str:\t%s\nlower:\t", str);
-	while (str[index])
-	{
-		putchar(ft_tolower(str[index]));
-		index++;
-	}
-	putchar('\n');
-	return (0);
-}
-*/
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,12 +6,11 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:31:52 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 15:17:39 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 21:22:14 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 static int	ft_islower(int c)
 {
@@ -24,22 +23,3 @@ int	ft_toupper(int c)
 		return (c - 'a' + 'A');
 	return (c);
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-	int		index;
-
-	index = 0;
-	str = "HeLlO";
-	printf("str:\t%s\nupper:\t", str);
-	while (str[index])
-	{
-		putchar(ft_toupper(str[index]));
-		index++;
-	}
-	putchar('\n');
-	return (0);
-}
-*/
