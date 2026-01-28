2117d8e67c533f37b9166c68538336f0ddfcae43
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 20:15:24 +0300

    isdigit: removing commented test

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,29 +6,13 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:31:30 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 15:16:10 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 19:48:04 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 int	ft_isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-
-	str = "he110 w0r1d!";
-	while (*str)
-	{
-		printf("[%c]\t%i\n", *str, ft_isdigit(*str));
-		str++;
-	}
-	return (0);
-}
-*/
