fac601be33d933a82393e48b76fff746c09cccae
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 21:51:08 +0300

    strchr: removing commented test & using iterative logic

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,39 +6,26 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:45:21 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 10:44:59 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:00:10 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*res;
+	size_t		index;
+	const char	*res;
 
 	res = 0;
-	if (*s != 0)
-		res = ft_strrchr(s + 1, c);
-	if (res == 0 && *s == (char)c)
-		res = (char *)s;
-	return (res);
+	index = 0;
+	while (s[index])
+	{
+		if (s[index] == (char)c)
+			res = &s[index];
+		index++;
+	}
+	if (s[index] == (char)c)
+		res = &s[index];
+	return ((char *)res);
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-	char	*res;
-	char	c;
-
-	c = 'o';
-	str = "Hello world";
-	res = ft_strrchr(str, c);
-	printf("chr: last [%c] in [%s] at [%p]\n", c, str, res);
-	res = strrchr(str, c);
-	printf("chr: last [%c] in [%s] at [%p]\n", c, str, res);
-	return (0);
-}
-*/
