31b0266f59613132599698ce156baa20a06d94e6
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 22:06:02 +0300

    memchr: relocating the return statment

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 13:14:56 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/28 22:03:19 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:04:49 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	index;
+	void	*res;
 
+	res = 0;
 	index = 0;
-	while (index < n)
+	while (index < n && res == 0)
 	{
 		if (((char *)s)[index] == (char)c)
-			return ((char *)s + index);
+			res = ((char *)s + index);
 		index++;
 	}
-	return (0);
+	return (res);
 }
