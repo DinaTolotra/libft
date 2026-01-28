a4eddaa78843db62f4205a1493c4be94f0d474a4
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 20:34:56 +0300

    isascii: removing commented test

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,29 +6,13 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:31:24 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/22 15:16:06 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 20:34:40 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
 	return (c >= 0 && c <= 127);
 }
-
-/*
-int	main(void)
-{
-	char	*str;
-
-	str = "he110 w0r1d!\1";
-	while (*str)
-	{
-		printf("[%c]\t%i\n", *str, ft_isascii(*str));
-		str++;
-	}
-	return (0);
-}
-*/
