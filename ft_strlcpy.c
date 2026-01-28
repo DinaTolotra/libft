95a4a1648f187cb960858ae683224ebc5b8fc88e
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 21:12:44 +0300

    strlcpy: removing commented test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,14 +6,11 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:43:26 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 09:40:33 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 21:07:56 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <bsd/string.h>
-//#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 int	ft_strlcpy(char *dst, const char *src, size_t size)
 {
@@ -32,18 +29,3 @@ int	ft_strlcpy(char *dst, const char *src, size_t size)
 	dst[index] = 0;
 	return (len);
 }
-
-/*
-int	main(void)
-{
-	char	str[2];
-	int		ret;
-
-	ret = strlcpy(str, "Hello", 0);
-	printf("str: [%s] [%i]\n", str, ret);
-	ret = strlcpy(str, "", 0);
-	ret = ft_strlcpy(str, "Hello", 0);
-	printf("str: [%s] [%i]\n", str, ret);
-	return (0);
-}
-*/
