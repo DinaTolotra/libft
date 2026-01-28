40a02e69a27fa50a1087b334698e6c7a98cbf151
todina-r <ratsinjomananatolotra@gmail.com>
2026-01-28 22:02:42 +0300

    strncmp: removing commented test

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,11 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:45:34 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 13:06:35 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/28 21:57:18 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-#include <stdio.h>
-#include "./libft.h"
+#include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -28,63 +26,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (res);
 }
-
-/*
-int	main(void)
-{
-	char	*s1;
-	char	*s2;
-	int		n;
-
-	n = 3;
-	s1 = "abc";
-	s2 = "abc";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 3;
-	s1 = "abc";
-	s2 = "abC";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 3;
-	s1 = "abc";
-	s2 = "aBc";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 2;
-	s1 = "abc";
-	s2 = "abc";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 2;
-	s1 = "abc";
-	s2 = "abC";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 2;
-	s1 = "abc";
-	s2 = "aBc";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	n = 2;
-	s1 = "abc";
-	s2 = "Abc";
-	printf("cmp: [%s] [%s] [%i] [%i] == [%i]\n",
-		s1, s2, n,
-		ft_strncmp(s1, s2, n),
-		strncmp(s1, s2, n));
-	return (0);
-}
-*/
