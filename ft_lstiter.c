38e92404404c7b1332aaedc2c75840b533bd869d
DinaTolotra <ratsinjomananatolotra@gmail.com>
2026-01-30 16:28:46 +0300

    cleaning lstiter and restructuring the code

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,18 +6,17 @@
 /*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 23:44:28 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/27 22:34:39 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/01/30 15:15:17 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft.h"
+#include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (lst)
+	if (lst && f)
 	{
-		if (f)
-			f(lst->content);
+		f(lst->content);
 		ft_lstiter(lst->next, f);
 	}
 }
