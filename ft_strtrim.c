14097f37bfcaa7b26e7f500d42effc0af742db08
todina-r <ratsinjomananatolotra@gmail.com>
2026-02-04 06:17:55 +0300

    Code cleaning

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:30:48 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/29 11:55:35 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/02/04 05:42:12 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static int	last_not_in(const char *s, const char *set)
 
 	stop = 0;
 	index = ft_strlen(s) - 1;
-	while (!stop && index > 0)
+	while (!stop && index >= 0)
 	{
 		if (ft_strchr(set, s[index]) == 0)
 			stop = 1;
@@ -50,15 +50,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*res;
 	int		res_len;
-	int		start;
+	int		begin;
+	int		end;
 
 	if (s1 == 0 || set == 0)
 		return (0);
-	start = first_not_in(s1, set);
-	res_len = last_not_in(s1, set);
-	res_len = res_len - start + 1;
-	if (res_len < 0)
+	begin = first_not_in(s1, set);
+	end = last_not_in(s1, set);
+	res_len = end - begin + 1;
+	if (end < begin)
 		res_len = 0;
-	res = ft_substr(s1, start, res_len);
+	res = ft_substr(s1, begin, res_len);
 	return (res);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: todina-r <todina-r@student.42antananarivo  +#+  +:+       +#+        */
+/*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 15:57:18 by todina-r          #+#    #+#             */
-/*   Updated: 2026/01/28 22:33:19 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/02/04 05:42:45 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*sub_s;
 	size_t	s_len;
 
-	sub_s = 0;
 	if (s == 0)
 		return (0);
 	s_len = ft_strlen(s);
-	if (s_len - start < len)
-		len = s_len - start;
 	if (s_len < start)
 		len = 0;
+	else if (s_len - start < len)
+		len = s_len - start;
 	sub_s = malloc(sizeof(char) * (len + 1));
-	if (sub_s && start < s_len)
-		ft_strlcpy(sub_s, s + start, len + 1);
-	if (sub_s)
-		sub_s[len] = 0;
+	if (sub_s == 0)
+		return (0);
+	ft_strlcpy(sub_s, s + start, len + 1);
+	sub_s[len] = 0;
 	return (sub_s);
 }
