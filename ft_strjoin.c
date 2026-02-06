0c7c79007f8b16ce6104216f6e52397944b43ccd
todina-r <ratsinjomananatolotra@gmail.com>
2026-02-06 12:16:17 +0300

    strjoin improve logic by using only strlcpy

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:17:33 by todina-r          #+#    #+#             */
-/*   Updated: 2026/02/04 11:20:57 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/02/06 12:16:56 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
-	size_t	len;
+	size_t	s1_len;
+	size_t	s2_len;
 
+	s1_len = 0;
 	if (s1 == 0 || s2 == 0)
 		return (0);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	res = malloc(sizeof(char) * (len + 1));
-	if (res)
-	{
-		ft_strlcpy(res, s1, len + 1);
-		ft_strlcat(res, s2, len + 1);
-	}
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = malloc(sizeof(char) * (s2_len + s1_len + 1));
+	if (res == 0)
+		return (0);
+	ft_strlcpy(res, s1, s2_len + s1_len + 1);
+	ft_strlcpy(res + s1_len, s2, s2_len + 1);
 	return (res);
 }
