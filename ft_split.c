9d52e78bfe256eeca47aa047e78d5fa7f6ae0eaa
todina-r <ratsinjomananatolotra@gmail.com>
2026-02-06 09:46:16 +0300

    split handle malloc error in ft_substr

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:49:36 by todina-r          #+#    #+#             */
-/*   Updated: 2026/02/04 12:27:56 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/02/06 09:41:05 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,31 +35,60 @@ static int	count_separated(char const *s, char c)
 	return (count);
 }
 
+void	free_vs(char ***p_vs)
+{
+	size_t	index;
+
+	index = 0;
+	while ((*p_vs)[index])
+	{
+		free((*p_vs)[index]);
+		(*p_vs)[index] = 0;
+		index++;
+	}
+	free(*p_vs);
+	*p_vs = 0;
+}
+
+size_t	get_word_len(const char *s, char c)
+{
+	size_t		len;
+	const char	*p_c;
+
+	len = 0;
+	p_c = ft_strchr(s, c);
+	if (p_c)
+		len = p_c - s;
+	else
+		len = ft_strchr(s, 0) - s;
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**res;
-	int		count;
+	size_t	count;
 	size_t	res_i;
-	int		beg;
-	size_t	end;
+	size_t	w_len;
+	char	*sub_s;
 
-	end = 0;
-	beg = -1;
 	res_i = 0;
 	if (s == 0)
 		return (0);
 	count = count_separated(s, c);
 	res = ft_calloc(sizeof(char *), (count + 1));
-	while (res && (s[end] || beg != -1))
+	while (res && res_i < count)
 	{
-		if (s[end] != c && beg == -1)
-			beg = end;
-		if ((s[end] == 0 || s[end] == c) && beg != -1)
+		w_len = get_word_len(s, c);
+		if (w_len)
 		{
-			res[res_i++] = ft_substr(s, beg, end - beg);
-			beg = -1;
+			sub_s = ft_substr(s, 0, w_len);
+			if (sub_s == 0)
+				free_vs(&res);
+			else
+				res[res_i++] = sub_s;
 		}
-		end = end + (s[end] != 0);
+		s = s + w_len + 1;
 	}
 	return (res);
 }
