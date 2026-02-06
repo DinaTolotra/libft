251f134c7564e808c305e0fc8900a4607039fb64
todina-r <ratsinjomananatolotra@gmail.com>
2026-02-06 10:30:41 +0300

    substr index guard

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 15:57:18 by todina-r          #+#    #+#             */
-/*   Updated: 2026/02/04 05:42:45 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/02/06 10:27:28 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (0);
 	s_len = ft_strlen(s);
 	if (s_len < start)
+	{
 		len = 0;
+		start = s_len;
+	}
 	else if (s_len - start < len)
 		len = s_len - start;
 	sub_s = malloc(sizeof(char) * (len + 1));
