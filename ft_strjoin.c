7d41435d98b988e22d509dad66dc4709ce3be0ea
todina-r <ratsinjomananatolotra@gmail.com>
2026-03-04 07:29:08 +0300

    ft_strjoin does not stop when called with null

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: todina-r <todina-r@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 16:17:33 by todina-r          #+#    #+#             */
-/*   Updated: 2026/02/06 12:16:56 by todina-r         ###   ########.fr       */
+/*   Updated: 2026/03/04 07:27:35 by todina-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 
 	s1_len = 0;
-	if (s1 == 0 || s2 == 0)
+	s2_len = 0;
+	if (s1 == 0 && s2 == 0)
 		return (0);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
+	if (s1)
+		s1_len = ft_strlen(s1);
+	if (s2)
+		s2_len = ft_strlen(s2);
 	res = malloc(sizeof(char) * (s2_len + s1_len + 1));
 	if (res == 0)
 		return (0);
-	ft_strlcpy(res, s1, s2_len + s1_len + 1);
-	ft_strlcpy(res + s1_len, s2, s2_len + 1);
+	if (s1)
+		ft_strlcpy(res, s1, s2_len + s1_len + 1);
+	if (s2)
+		ft_strlcpy(res + s1_len, s2, s2_len + 1);
+	res[s1_len + s2_len] = 0;
 	return (res);
 }
